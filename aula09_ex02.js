d7e703c3a6d434bd40f6bd9e22983e900de29fb4
Vtrsouza1998 <vitordorea2015@gmail.com>
2018-11-01 11:17:49 -0300

    inclusao

---
+++ aula09_ex02.js
@@ -0,0 +1,23 @@
+
+var usuario = "";
+var senha = "";
+
+function Verifica(){
+	usuario = document.getElementById('txtUsuario').value;
+	senha = document.getElementById('txtSenha').value;
+	if((usuario=="")||(senha=="")){ 
+		alert("Campo(s) Vazio(s)!!!");
+	}else{ 
+		if((usuario=="aluno")&&(senha=="1234")){
+			window.location.href = "aula09_ex01.html";
+		}else{
+			alert("Usuario e/ou Senha Inválido(s)!!!");
+			LimparCampos();
+		}
+	}
+}
+
+function LimparCampos(){
+	document.getElementById('txtUsuario').value = "";
+	document.getElementById('txtSenha').value = "";
+}
